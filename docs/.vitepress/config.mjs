69967097ae16b59fd4f7b101590fec3e8d5270ee
Nicolas Neudeck <nicolas.neudeck@outlook.de>
2024-04-11 09:17:31 +0200

    make favicon fill link

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -21,7 +21,7 @@ export default defineConfig({
     }
   },
   head: [
-    [ 'link', { rel: 'icon', href: '/favicon.ico' } ],
+    [ 'link', { rel: 'icon', href: 'https://nicolasneudeck.com/favicon.ico' } ],
   ],
   sitemap: {
     hostname: 'https://nicolasneudeck.com',
